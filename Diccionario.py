75c5f8192b86b5c48316b5807bbb8b2b23966e20
Taala2015 <pablo.hernandez19820000@gmail.com>
2025-09-12 17:12:18 -0400

    Add files via upload

---
+++ Diccionario.py
@@ -0,0 +1,10 @@
+mi_diccionario = {
+    "nombre": "pablo" ,
+    "edad" : 43,
+    "cuidad" : "maracaibo" ,
+    "color favorito" : "azul" ,
+}
+
+
+for clave, valor in mi_diccionario.items():
+    print(f"\nClave:{clave} valor: {valor}")
---
+++ bucles.py
@@ -0,0 +1,8 @@
+numero = 0
+for numero in range (11):
+    print(f"numero: {numero}")
+
+
+contador = 0
+while contador > 10 :
+    print(f"contador:{contador}")
